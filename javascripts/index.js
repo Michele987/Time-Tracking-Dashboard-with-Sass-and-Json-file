627d63fc4727835fa30ec64fd4e58338f9a5d29a
Michele Pugliese <michele.mona987@gmail.com>
2022-02-10 18:21:56 +0100

    Gestione asincrona relativa ai dati delle giornate effettuata con successo, manca la stilizzazione dei seguenti dati.. inoltre devo solo cancellare i puntini delle opzioni e aggiungerlo dinamicamente con Javascript al click.

---
+++ javascripts/index.js
@@ -0,0 +1,58 @@
+let daily = document.getElementById("daily");
+
+daily.addEventListener("click",function(){
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+
+        if(xhr.readyState == 4)
+        if(xhr.status == 200){
+
+            const success = JSON.parse(xhr.responseText);
+            console.log(success)
+
+            document.querySelector("#work .hobby p").innerHTML = success[0].title;
+            document.querySelector("#work  #data1").innerHTML = success[0].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#work  #data2").innerHTML = "<p> Last Week - </p> " + success[0].timeframes.daily.previous + "<p> hrs </p>";
+
+            document.querySelector("#play .hobby p").innerHTML = success[1].title;
+            document.querySelector("#play  #data1").innerHTML = success[1].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#play  #data2").innerHTML = "<p> Last Week - </p> " + success[1].timeframes.daily.previous + "<p> hrs </p>";
+
+            document.querySelector("#study .hobby p").innerHTML = success[2].title;
+            document.querySelector("#study #data1").innerHTML = success[2].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#study  #data2").innerHTML = "<p> Last Week - </p> " + success[2].timeframes.daily.previous + "<p> hrs </p>";
+
+            document.querySelector("#exercise .hobby p").innerHTML = success[3].title;
+            document.querySelector("#exercise #data1").innerHTML = success[3].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#exercise  #data2").innerHTML = "<p> Last Week - </p> " + success[3].timeframes.daily.previous + "<p> hrs </p>";
+
+            document.querySelector("#social .hobby p").innerHTML = success[4].title;
+            document.querySelector("#social #data1").innerHTML = success[4].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#social  #data2").innerHTML = "<p> Last Week - </p> " + success[4].timeframes.daily.previous + "<p> hrs </p>";
+
+            document.querySelector("#selfCare .hobby p").innerHTML = success[5].title;
+            document.querySelector("#selfCare #data1").innerHTML = success[5].timeframes.daily.current + "<p> hrs </p>";
+            document.querySelector("#selfCare  #data2").innerHTML = "<p> Last Week - </p> " + success[5].timeframes.daily.previous + "<p> hrs </p>";
+
+
+
+
+
+
+
+
+
+        } else{
+            const error = JSON.parse(xhr.responseText);
+            console.log(error)
+        }
+
+        
+    }
+
+        xhr.open('GET', '/data.json');
+        xhr.send();
+
+});
