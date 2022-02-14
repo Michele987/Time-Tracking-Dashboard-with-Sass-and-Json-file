baad6c2a467b7d2d35f2477b1a97b126aba7f879
Michele Pugliese <michele.mona987@gmail.com>
2022-02-14 11:00:21 +0100

    Risolto il problema della select, adesso i 3 puntini verranno visualizzati al click.

--- javascripts/index.js
+++ javascripts/index.js
@@ -15,34 +15,37 @@ daily.addEventListener("click",function(){
             document.querySelector("#work .hobby p").innerHTML = success[0].title;
             document.querySelector("#work  #data1").innerHTML = success[0].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#work  #data2").innerHTML = "<p> Last Week - </p> " + success[0].timeframes.daily.previous + "<p> hrs </p>";
+            document.querySelector("#work .hobby .select").innerHTML =  `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
 
             document.querySelector("#play .hobby p").innerHTML = success[1].title;
             document.querySelector("#play  #data1").innerHTML = success[1].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#play  #data2").innerHTML = "<p> Last Week - </p> " + success[1].timeframes.daily.previous + "<p> hrs </p>";
+            document.querySelector("#play .hobby .select").innerHTML = `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
+
+
 
             document.querySelector("#study .hobby p").innerHTML = success[2].title;
             document.querySelector("#study #data1").innerHTML = success[2].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#study  #data2").innerHTML = "<p> Last Week - </p> " + success[2].timeframes.daily.previous + "<p> hrs </p>";
+            document.querySelector("#study .hobby .select").innerHTML = `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
+
 
             document.querySelector("#exercise .hobby p").innerHTML = success[3].title;
             document.querySelector("#exercise #data1").innerHTML = success[3].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#exercise  #data2").innerHTML = "<p> Last Week - </p> " + success[3].timeframes.daily.previous + "<p> hrs </p>";
+            document.querySelector("#exercise .hobby .select").innerHTML = `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
+
 
             document.querySelector("#social .hobby p").innerHTML = success[4].title;
             document.querySelector("#social #data1").innerHTML = success[4].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#social  #data2").innerHTML = "<p> Last Week - </p> " + success[4].timeframes.daily.previous + "<p> hrs </p>";
+            document.querySelector("#social .hobby .select").innerHTML = `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
+
 
             document.querySelector("#selfCare .hobby p").innerHTML = success[5].title;
             document.querySelector("#selfCare #data1").innerHTML = success[5].timeframes.daily.current + "<p> hrs </p>";
             document.querySelector("#selfCare  #data2").innerHTML = "<p> Last Week - </p> " + success[5].timeframes.daily.previous + "<p> hrs </p>";
-
-
-
-
-
-
-
-
+            document.querySelector("#selfCare .hobby .select").innerHTML = `<div id="bar1"> </div> <div id="bar2"> </div> <div id="bar3"> </div> `;
 
         } else{
             const error = JSON.parse(xhr.responseText);
@@ -55,4 +58,10 @@ daily.addEventListener("click",function(){
         xhr.open('GET', '/data.json');
         xhr.send();
 
+        
+
 });
+
+
+
+
